b322173458f5eb3bb3e63cbaaa7e7f4920948c2b
Krishna Sundarram <krishna.sundarram@gmail.com>
2020-08-31 20:25:14 +0100

    Add site
    gatsby new site https://github.com/gatsbyjs/gatsby-starter-hello-world

---
+++ site/gatsby-config.js
@@ -0,0 +1,12 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  siteMetadata: {
+    title: `Are We Fast Yet`,
+  },
+  plugins: [],
+}
---
+++ site/src/pages/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Results from "../../static/results.json"
+
+
+export default function Home() {
+  console.log(Results);
+  return <div>Hello world!</div>
+}
