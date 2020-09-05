59cf12b809ea75027bef1d5e6dbb4a40dd96344a
Krishna Sundarram <krishna.sundarram@gmail.com>
2020-09-05 23:08:42 +0100

    Change next.js template to TypeScript

---
+++ site2/pages/_app.tsx
@@ -0,0 +1,9 @@
+import '../styles/globals.css'
+import type { AppProps /*, AppContext */ } from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+
+export default MyApp
--- site2/pages/index.tsx
+++ site2/pages/index.tsx
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Are We Fast Yet</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Are We Fast Yet!</a>
         </h1>
 
         <p className={styles.description}>
