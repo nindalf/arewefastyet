e54fa4a7c68e05ce4f44d830cd72f9c4e1c90d4d
Krishna Sundarram <krishna.sundarram@gmail.com>
2020-09-12 23:04:31 +0100

    Change result file name

--- collect_samples.sh
+++ collect_samples.sh
@@ -4,10 +4,11 @@
 # cd arewefastyet
 # ./collect_samples.sh
 
-sudo apt-get install -y build-essential
+sudo apt-get install -y build-essential nasm # nasm required for rav1e
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 
+CORES=$(cat /proc/cpuinfo | grep cores | head -n 1 | cut -d' ' -f3)
 cd cmd
 cargo build --release
-./target/release/arewefastyet --times 5 --results ../results.json
+./target/release/arewefastyet --times 5 --results ../data/results-$CORES.json
