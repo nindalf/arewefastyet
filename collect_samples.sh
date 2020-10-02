8c7a0786b6e897d7498ed176e30e6c2f421aee60
Krishna Sundarram <krishna.sundarram@gmail.com>
2020-10-02 20:33:27 +0100

    Update arguments

--- collect_samples.sh
+++ collect_samples.sh
@@ -16,7 +16,6 @@ sudo apt-get install -y libssl-dev pkg-config
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 
-CORES=$(cat /proc/cpuinfo | grep cores | head -n 1 | cut -d' ' -f3)
 cd cmd
 cargo build --release
-./target/release/arewefastyet --times 5 --results ../data/results-$CORES.json
+./target/release/arewefastyet --times 5 --results_dir ../data/
