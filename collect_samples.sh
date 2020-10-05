d3ca753278477919fc4904c6491dbc8582d19eb2
Krishna Sundarram <krishna.sundarram@gmail.com>
2020-10-05 11:47:58 +0100

    Switch to rustc stable before building cmd

--- collect_samples.sh
+++ collect_samples.sh
@@ -15,6 +15,7 @@ sudo apt-get install -y libssl-dev pkg-config
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
+rustup default stable
 
 cd cmd
 echo "Building arewefastyet"
